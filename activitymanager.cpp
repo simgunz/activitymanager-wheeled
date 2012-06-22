742bcea5b2821e24727cd3155f2fbafdb6bfba01
Simone Gaiarin <simgunz@gmail.com>
2012-06-22 23:09:28 +0200

    Modify copyright

--- activitymanager.cpp
+++ activitymanager.cpp
@@ -1,5 +1,6 @@
 /***************************************************************************
-* Copyright © 2010 Abdurrahman AVCI <abdurrahmanavci@gmail.com
+* Copyright © 2010 Abdurrahman AVCI <abdurrahmanavci@gmail.com>
+* Copyright © 2011 Simone Gaiarin <simgunz@gmail.com>
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
